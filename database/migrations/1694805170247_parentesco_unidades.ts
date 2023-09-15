d58f89dc8a0912f4ac366bc01225c95ab2f7b5a1
GiovaneMiranda15 <mirandagiovane7@gmail.com>
2023-09-15 16:54:01 -0400

    Criação das Migrations versão 1.0

--- database/migrations/1694805170247_parentesco_unidades.ts
+++ database/migrations/1694805170247_parentesco_unidades.ts
@@ -1,9 +1,9 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 /**
- * Classe de migração para criar a tabela 'plano_unidade'.
+ * Classe de migração para criar a tabela 'parentesco_unidade'.
  *
- * Esta migração verifica se a tabela 'plano_unidade' já existe no banco de dados.
+ * Esta migração verifica se a tabela 'parentesco_unidade' já existe no banco de dados.
  * Se não existir, a tabela é criada com as colunas especificadas.
  * Se já existir, nada é feito no método 'up'.
  *
@@ -25,12 +25,12 @@ export default class extends BaseSchema {
    * @protected
    * @type {string}
    */
-  protected tableName: string = 'plano_unidade'
+  protected tableName: string = 'parentesco_unidade'
 
   /**
    * Método 'up' da migração.
-   * Cria a tabela 'plano_unidade' se ela não existir.
-   *
+   * Cria a tabela 'parentesco_unidade' se ela não existir.
+   * 
    * @public
    * @returns {Promise<void>}
    */
@@ -42,30 +42,23 @@ export default class extends BaseSchema {
     if (!hasTable) {
       this.schema.withSchema(this.schemaName)
         .createTable(this.tableName, (table) => {
-          table.integer('plano_id').notNullable().unsigned().references('id').inTable('cobranca.plano').onDelete('CASCADE').onUpdate('CASCADE')
+          table.integer('parentesco_id').notNullable().unsigned().references('id').inTable('public.parentesco').onDelete('CASCADE').onUpdate('CASCADE')
           table.integer('unidade_id').notNullable().unsigned().references('id').inTable('public.unidade').onDelete('CASCADE').onUpdate('CASCADE')
-          table.decimal('valor_adesao', 10, 2).notNullable().defaultTo(0)
-          table.decimal('valor_mensalidade', 10, 2).notNullable().defaultTo(0)
-          table.decimal('valor_cartao', 10, 2).nullable().defaultTo(0)
-          table.decimal('valor_adicional', 10, 2).notNullable().defaultTo(0)
-          table.decimal('valor_transferencia', 10, 2).notNullable().defaultTo(0)
-          table.integer('carencia_novo').nullable().defaultTo(0)
-          table.integer('carencia_atraso').nullable().defaultTo(0)
-          table.integer('limite_dependente').nullable().defaultTo(0)
+          table.boolean('adicional').notNullable()
           table.boolean('ativo').notNullable().defaultTo(true).comment('Se valor for TRUE o mesmo não aparece nas listagens, exceto nas rotas de busca geral.')
           table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
           table.string('created_by', 150).notNullable()
           table.timestamp('updated_at', { useTz: true }).nullable()
           table.string('updated_by', 150).nullable()
 
-          table.primary(['plano_id', 'unidade_id'])
+          table.primary(['parentesco_id', 'unidade_id'])
         })
     }
   }
 
   /**
    * Método 'down' da migração.
-   * Exclui a tabela 'plano_unidade' se ela existir.
+   * Exclui a tabela 'parentesco_unidade' se ela existir.
    *
    * @public
    * @returns {Promise<void>}
--- database/migrations/1694806128743_plano_items.ts
+++ database/migrations/1694806128743_plano_items.ts
@@ -1,9 +1,9 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 /**
- * Classe de migração para criar a tabela 'plano_unidade'.
+ * Classe de migração para criar a tabela 'plano_item'.
  *
- * Esta migração verifica se a tabela 'plano_unidade' já existe no banco de dados.
+ * Esta migração verifica se a tabela 'plano_item' já existe no banco de dados.
  * Se não existir, a tabela é criada com as colunas especificadas.
  * Se já existir, nada é feito no método 'up'.
  *
@@ -25,12 +25,14 @@ export default class extends BaseSchema {
    * @protected
    * @type {string}
    */
-  protected tableName: string = 'plano_unidade'
+  protected tableName: string = 'plano_item'
 
   /**
    * Método 'up' da migração.
-   * Cria a tabela 'plano_unidade' se ela não existir.
-   *
+   * Cria a tabela 'plano_item' se ela não existir.
+   * 
+   * Essa tabela é apenas para fins de auxílio. Ao cadastrar um associado com determinado plano, esses valores irão ser pré-carregados, para serem alterados e assim inseridos na tabela 'associado_item'.
+   * 
    * @public
    * @returns {Promise<void>}
    */
@@ -42,30 +44,26 @@ export default class extends BaseSchema {
     if (!hasTable) {
       this.schema.withSchema(this.schemaName)
         .createTable(this.tableName, (table) => {
-          table.integer('plano_id').notNullable().unsigned().references('id').inTable('cobranca.plano').onDelete('CASCADE').onUpdate('CASCADE')
+          table.integer('plano_id').nullable().unsigned().references('id').inTable('cobranca.plano').onDelete('SET NULL').onUpdate('SET NULL')
+          table.integer('item_id').notNullable().unsigned().references('id').inTable('public.item').onDelete('CASCADE').onUpdate('CASCADE')
           table.integer('unidade_id').notNullable().unsigned().references('id').inTable('public.unidade').onDelete('CASCADE').onUpdate('CASCADE')
+          table.integer('quantidade').notNullable().defaultTo(1)
           table.decimal('valor_adesao', 10, 2).notNullable().defaultTo(0)
           table.decimal('valor_mensalidade', 10, 2).notNullable().defaultTo(0)
-          table.decimal('valor_cartao', 10, 2).nullable().defaultTo(0)
-          table.decimal('valor_adicional', 10, 2).notNullable().defaultTo(0)
-          table.decimal('valor_transferencia', 10, 2).notNullable().defaultTo(0)
-          table.integer('carencia_novo').nullable().defaultTo(0)
-          table.integer('carencia_atraso').nullable().defaultTo(0)
-          table.integer('limite_dependente').nullable().defaultTo(0)
           table.boolean('ativo').notNullable().defaultTo(true).comment('Se valor for TRUE o mesmo não aparece nas listagens, exceto nas rotas de busca geral.')
           table.timestamp('created_at', { useTz: true }).notNullable().defaultTo(this.now())
           table.string('created_by', 150).notNullable()
           table.timestamp('updated_at', { useTz: true }).nullable()
           table.string('updated_by', 150).nullable()
 
-          table.primary(['plano_id', 'unidade_id'])
+          table.unique(['plano_id', 'item_id', 'unidade_id'])
         })
     }
   }
 
   /**
    * Método 'down' da migração.
-   * Exclui a tabela 'plano_unidade' se ela existir.
+   * Exclui a tabela 'plano_item' se ela existir.
    *
    * @public
    * @returns {Promise<void>}
